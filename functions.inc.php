cf4c3bdd2aad2fc240bf3c9764daaa1aedf8afdc
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-06-17 18:55:35 +0000

    use jquery to hide the context field when a custom context select box is being displayed, avoids confusion

--- functions.inc.php
+++ functions.inc.php
@@ -384,6 +384,9 @@ function customcontexts_devices_configpageload() {
   $curcontext = $_REQUEST['customcontext'];
 
 	$currentcomponent->addguielem('Device Options', new gui_selectbox('customcontext', $currentcomponent->getoptlist('contextssel'), $curcontext, 'Custom Context', 'You have the '.customcontexts_getmodulevalue('moduledisplayname').' Module installed! You can select a custom context from this list to limit this user to portions of the dialplan you defined in the '.customcontexts_getmodulevalue('moduledisplayname').' module.',true, "javascript:if (document.frm_devices.customcontext.value) {document.frm_devices.devinfo_context.value = document.frm_devices.customcontext.value} else {document.frm_devices.devinfo_context.value = 'from-internal'}"));
+
+  $js = '<script type="text/javascript">$(document).ready(function(){$("#devinfo_context").parent().parent().hide();});</script>';
+  $currentcomponent->addguielem('Device Options', new guielement('test-html', $js, ''));
 }
 
 //hook gui function
@@ -395,6 +398,9 @@ function customcontexts_extensions_configpageload() {
   $curcontext = $_REQUEST['customcontext'];
 
 	$currentcomponent->addguielem('Device Options', new gui_selectbox('customcontext', $currentcomponent->getoptlist('contextssel'), $curcontext, 'Custom Context', 'You have the '.customcontexts_getmodulevalue('moduledisplayname').' Module installed! You can select a custom context from this list to limit this user to portions of the dialplan you defined in the '.customcontexts_getmodulevalue('moduledisplayname').' module.',true, "javascript:if (document.frm_extensions.customcontext.value) {document.frm_extensions.devinfo_context.value = document.frm_extensions.customcontext.value} else {document.frm_extensions.devinfo_context.value = 'from-internal'}"));
+
+  $js = '<script type="text/javascript">$(document).ready(function(){$("#devinfo_context").parent().parent().hide();});</script>';
+  $currentcomponent->addguielem('Device Options', new guielement('test-html', $js, ''));
 }
 
 /*
