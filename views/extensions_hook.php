40f1013ff29145c570d83b6f9fca51e7899cb8c4
Andrew Nagy <andrew.nagy@the159.com>
2018-07-13 05:37:08 +0000

    FREEPBX-17779

--- views/extensions_hook.php
+++ views/extensions_hook.php
@@ -36,5 +36,5 @@ if ( $_REQUEST['display'] == 'extensions' ) {
 
 $currentcomponent->setoptlistopts('contextssel', 'sort', false);
 $currentcomponent->addguielem($section, new gui_selectbox('customcontext', $currentcomponent->getoptlist('contextssel'), $curcontext, _('Custom Context'), sprintf(_('You can select a custom context from this list to limit this user to portions of the dialplan you defined in the %s module.'),customcontexts_getmodulevalue('moduledisplayname')),false), 4, null, $category);
-$js = '<script type="text/javascript" id="customcontext_js">$("#devinfo_context").parents(".element-container").remove();$("#customcontext").attr("name","devinfo_context");$("#customcontext_js").parents(".element-container").remove();</script>';
+$js = '<script type="text/javascript" id="customcontext_js">$(document).ready(function() {$("#devinfo_context").parents(".element-container").remove();$("#customcontext").attr("name","devinfo_context");$("#customcontext_js").parents(".element-container").remove();});</script>';
 $currentcomponent->addguielem($section, new guielement('test-html', $js, ''),4, null, $category);
